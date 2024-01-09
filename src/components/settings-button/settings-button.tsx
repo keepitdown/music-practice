0808eeb39e5fc3464b19cf3dc6de2e75ad8bd37d
Artem <mntp112@yandex.ru>
2024-01-09 20:06:18 +0300

    feat: settings-button hover animation update & click animation

--- src/components/settings-button/settings-button.tsx
+++ src/components/settings-button/settings-button.tsx
@@ -1,19 +1,29 @@
 import Image from 'next/image'
-import settingsIcon from '@/images/settings-icon.svg'
+import settingsIcon from '@/images/settings-icon-cropped.svg'
 import styles from './settings-button.module.css'
+import { AnimationEventHandler, MouseEventHandler } from 'react';
 
 export default function SettingsButton({ onClick, extraClass, disabled }: { onClick: () => void, extraClass?: string, disabled?: boolean }) {
+  
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    onClick();
+    e.currentTarget.classList.add(styles.pushed);
+  };
+
+  const handleAnimationEnd: AnimationEventHandler<HTMLButtonElement> = (e) => e.currentTarget.classList.remove(styles.pushed);
+
   return (
     <button
       type="button"
-      onClick={onClick}
+      onClick={handleClick}
+      onAnimationEnd={handleAnimationEnd}
       className={styles.button + (extraClass ? (' ' + extraClass) : '')}
       disabled={disabled}
       >
       <Image
         src={settingsIcon}
         alt="Показать настройки"
-        className={styles.image}
+        className={styles.buttonImage}
       />
     </ button>
   )
