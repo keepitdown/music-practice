226ce5fbc50ddf125a24c568f01379f12e1bc4e9
Artem <mntp112@yandex.ru>
2024-01-11 21:25:37 +0300

    feat: shuffle-button click animation

--- src/components/shuffle-button/shuffle-button.tsx
+++ src/components/shuffle-button/shuffle-button.tsx
@@ -3,13 +3,30 @@ import Button from '../button/button'
 import styles from './shuffle-button.module.css'
 import Image from 'next/image'
 import shuffleIcon from '@/images/repeat-icon-2-cropped.svg'
+import { AnimationEventHandler, MouseEventHandler } from 'react'
 
-export default function ShuffleButton() {
+type TShuffleButton = {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+// TODO: pass prop onClick from parent component or remove
+
+export default function ShuffleButton({ onClick }: TShuffleButton) {
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.currentTarget.classList.add(styles.pressed);
+  };
+
+  const handleAnimationEnd: AnimationEventHandler<HTMLButtonElement> = (e) => {
+    e.currentTarget.classList.remove(styles.pressed);
+  };
+
   return (
     <button
       type="button"
       className={styles.button}
-      onClick={() => { }}
+      onClick={handleClick}
+      onAnimationEnd={handleAnimationEnd}
     >
       <Image width={50} src={shuffleIcon} alt="Перемешать" className={styles.buttonImage} />
     </button>
