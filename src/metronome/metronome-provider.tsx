4025670f554651a60ba5ab40ca0979aea3cadcb8
Artem <mntp112@yandex.ru>
2024-02-01 06:10:41 +0300

    feat: metronome basic continious playback

--- src/metronome/metronome-provider.tsx
+++ src/metronome/metronome-provider.tsx
@@ -5,7 +5,19 @@ type TMetronomeProvider = {
   children: ReactNode;
 }
 
-function schedulePulse({ context, beatBuffer, downBeatBuffer, tempo, schedulingWindow }: any) {
+function schedulePlayback({ downbeatBuffer, beatBuffer }: { [bufferName: string]: AudioBuffer }, tempo: number, beatsPerBar: number, context: AudioContext) {
+  const beatDuration = 60 / tempo;
+  const scheduleAfterMilSec = 25;
+  const scheduleAheadSec = 0.1;
+  let nextBeat = 0;
+
+  let nextNote = context.currentTime + beatDuration;
+  while (nextNote <= context.currentTime + scheduleAheadSec) {
+    const sampleBuffer = (nextBeat === 0) ? downbeatBuffer : beatBuffer;
+    playSample(sampleBuffer, context, nextNote);
+    nextBeat = (nextBeat + 1) % beatsPerBar;
+    nextNote += beatDuration;
+  }
 
 }
 
@@ -16,13 +28,11 @@ async function loadSample(sampleUrl: string, context: AudioContext) {
   return audioBuffer;
 }
 
-function playSample(sampleBuffer: AudioBuffer, audioContext: AudioContext) {
+function playSample(sampleBuffer: AudioBuffer, audioContext: AudioContext, time: number = 0) {
   const source = audioContext.createBufferSource();
   source.buffer = sampleBuffer;
   source.connect(audioContext.destination);
-  source.start();
-  // TODO: Remove log
-  console.log('beep');
+  source.start(time);
 }
 
 const MetronomeContext = createContext(null);
@@ -66,18 +76,47 @@ export default function MetronomeProvider({ children }: TMetronomeProvider) {
     };
   }, [isTurnedOn, tempo]); */
 
-  const buttonHandler: MouseEventHandler<HTMLButtonElement> = () => {
+  useEffect(() => {
     const audioContext = audioContextRef.current as AudioContext;
     const sampleBuffer = sampleBufferRef.current as AudioBuffer;
-    /* if (audioContext?.state === "suspended") {
+    if (audioContext?.state === "suspended") {
       audioContext.resume();
-    } */
-    playSample(sampleBuffer, audioContext);
-  };
+    }
+    // playSample(sampleBuffer, audioContext);
+    const beatDuration = 60 / tempo;
+    const scheduleAfterMilSec = 25;
+    const scheduleAheadSec = 0.1;
+    let nextBeat = 0;
+    //TODO: find out if overhead is needed
+    let nextNote = audioContext.currentTime;
+    let timerId: ReturnType<typeof setTimeout>;
+
+    function schedulePlayback() {
+      while (nextNote <= audioContext.currentTime + scheduleAheadSec) {
+        // const sampleBuffer = (nextBeat === 0) ? downbeatBuffer : beatBuffer;
+        playSample(sampleBuffer, audioContext, nextNote);
+        nextBeat = (nextBeat + 1) % 4;
+        nextNote += beatDuration;
+      }
+      timerId = setTimeout(schedulePlayback, scheduleAfterMilSec);
+    }
+    if (isTurnedOn) {
+      console.log('start');
+      schedulePlayback();
+    }
+    return () => {
+      console.log('stop');
+      clearTimeout(timerId);
+    }
+  }, [isTurnedOn]);
+
+  const buttonHandler: MouseEventHandler<HTMLButtonElement> = () => {
+    setIsTurnedOn(value => !value);
+  }
 
   return (
     <MetronomeContext.Provider value={null}>
-      <button onClick={buttonHandler}>Beep</button>
+      <button onClick={buttonHandler} style={{ position: 'fixed', left: 30, bottom: 30, fontSize: 16, padding: 5 }}>Beep</button>
       {children}
     </MetronomeContext.Provider>
   );
