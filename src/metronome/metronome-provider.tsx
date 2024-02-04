05b343cc889ca09219eb14fb118e770076bcc899
Artem <mntp112@yandex.ru>
2024-02-04 18:25:38 +0300

    refactor: move metronome logic from react component to object class

--- src/metronome/metronome-provider.tsx
+++ src/metronome/metronome-provider.tsx
@@ -1,40 +1,6 @@
 'use client'
 import { MouseEventHandler, ReactNode, createContext, useEffect, useRef, useState } from "react"
-import config from "./metronome.config.json"
-
-function loadSamples(sampleUrls: string[], audioContext: AudioContext) {
-  const bufferPromises = sampleUrls.map(async url => {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-    return audioBuffer;
-  });
-  return Promise.all(bufferPromises);
-}
-
-function playSample(sampleBuffer: AudioBuffer, audioContext: AudioContext, time?: number) {
-  const source = audioContext.createBufferSource();
-  source.buffer = sampleBuffer;
-  source.connect(audioContext.destination);
-  source.start(time);
-}
-
-function createScheduler(audioContext: AudioContext, sampleBuffer: AudioBuffer, tempo: number, startTime: number) {
-  const beatDuration = 60 / tempo;
-  const scheduleAheadSec = config.scheduleAheadMilSec / 1000;
-  let nextBeat = 0;
-  let nextNoteTime = startTime;
-
-  function schedulePlayback() {
-    while (nextNoteTime <= audioContext.currentTime + scheduleAheadSec) {
-      // const sampleBuffer = (nextBeat === 0) ? downbeatBuffer : beatBuffer;
-      playSample(sampleBuffer, audioContext, nextNoteTime);
-      nextBeat = (nextBeat + 1) % 4; // TODO: replace 4 with a betsPerBar parameter
-      nextNoteTime += beatDuration;
-    }
-  }
-  return schedulePlayback;
-}
+import Metronome from "./metronome";
 
 type TMetronomeProvider = {
   children: ReactNode;
@@ -44,8 +10,7 @@ const MetronomeContext = createContext(null);
 
 export default function MetronomeProvider({ children }: TMetronomeProvider) {
 
-  const audioContextRef = useRef<AudioContext | null>(null);
-  const sampleBufferRef = useRef<AudioBuffer | null>(null);
+  const metronomeRef = useRef<Metronome | null>(null);
 
   const [isTurnedOn, setIsTurnedOn] = useState(false);
   // TODO: add default tempo config option or component prop
@@ -53,39 +18,19 @@ export default function MetronomeProvider({ children }: TMetronomeProvider) {
   const [status, setStatus] = useState<'loading' | 'ready' | 'error'>('loading');
 
   useEffect(() => {
-    audioContextRef.current = new AudioContext();
-    const audioContext = audioContextRef.current;
-    loadSamples(['/audio/downbeat-click.wav', '/audio/beat-click.wav'], audioContext)
-      .then(sampleBuffers => {
-        sampleBufferRef.current = sampleBuffers[0];
-      })
-    return () => {
-      audioContext.close();
-    };
+    metronomeRef.current = new Metronome();
+    metronomeRef.current.loadSamples();
+    return () => metronomeRef.current?.remove();
   }, []);
 
   useEffect(() => {
-    console.log('effect')
-    const audioContext = audioContextRef.current as AudioContext;
-    const sampleBuffer = sampleBufferRef.current as AudioBuffer;
-    if (audioContext?.state === 'suspended') {
-      audioContext.resume();
-    }
-
-    let timerId: ReturnType<typeof setTimeout>;
-    //NOTE: Inferred type is NodeJs.Timeout. Incorrect in this case, but not important.
-
     if (isTurnedOn) {
-      const schedulePlayback = createScheduler(audioContext, sampleBuffer, tempo, audioContext.currentTime);
-      const scheduleRepeatedly = () => {
-        schedulePlayback();
-        timerId = setTimeout(scheduleRepeatedly, config.scheduleAfterMilSec);
-      };
-      scheduleRepeatedly();
+      console.log('start');
+      metronomeRef.current?.start();
     }
     return () => {
       console.log('stop');
-      clearTimeout(timerId);
+      metronomeRef.current?.stop();
     }
   }, [isTurnedOn, tempo]);
 
---
+++ src/metronome/metronome.ts
@@ -0,0 +1,78 @@
+import config from './metronome.config.json'
+
+function loadSamples(sampleUrls: string[], audioContext: AudioContext) {
+  const bufferPromises = sampleUrls.map(async url => {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+  });
+  return Promise.all(bufferPromises);
+}
+
+function playSample(sampleBuffer: AudioBuffer, audioContext: AudioContext, time?: number) {
+  const source = audioContext.createBufferSource();
+  source.buffer = sampleBuffer;
+  source.connect(audioContext.destination);
+  source.start(time);
+}
+
+export default class Metronome {
+
+  constructor() { };
+
+  private context = new AudioContext();
+  tempo = 60;
+  private samples: AudioBuffer[] | null = null;
+  private beat = 0;
+  beatsPerBar: 2 | 3 | 4 = 4;
+
+  private nextNoteTime = 0; //TODO: replace init value
+  private timerId: ReturnType<typeof setTimeout> | undefined;
+
+  remove() {
+    this.context.close();
+  }
+
+  async loadSamples() {
+    try {
+      this.samples = await loadSamples(['/audio/downbeat-click.wav', '/audio/beat-click.wav'], this.context)
+      return 'success';
+    }
+    catch (error) {
+      return error;
+    }
+  }
+
+  start() {
+    if (this.context.state === 'suspended') {
+      this.context.resume();
+    }
+
+    this.beat = 0;
+    this.nextNoteTime = this.context.currentTime;
+
+    const schedulePlayback = (tempo: number) => {
+      const scheduleAheadSec = config.scheduleAheadMilSec / 1000;
+      const beatDuration = 60 / tempo;
+      while (this.nextNoteTime <= this.context.currentTime + scheduleAheadSec) {
+        // const sampleBuffer = (nextBeat === 0) ? downbeatBuffer : beatBuffer;
+        playSample(this.samples![0], this.context, this.nextNoteTime);
+        this.beat = (this.beat + 1) % this.beatsPerBar;
+        this.nextNoteTime += beatDuration;
+      }
+    }
+    const scheduleRepeatedly = () => {
+      schedulePlayback(this.tempo);
+      this.timerId = setTimeout(scheduleRepeatedly, config.scheduleAfterMilSec);
+    }
+    scheduleRepeatedly();
+  }
+  stop() {
+    clearTimeout(this.timerId);
+  }
+}
+
+
+
+const metro = new Metronome();
