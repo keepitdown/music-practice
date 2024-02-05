c3ca3d48903773c0a7a4b1b7b18763e33ac8dcd1
Artem <mntp112@yandex.ru>
2024-02-05 05:49:09 +0300

    refactor: rename private properties using underscore convention, add accessors for tempo

--- src/metronome/metronome.ts
+++ src/metronome/metronome.ts
@@ -19,23 +19,32 @@ function playSample(sampleBuffer: AudioBuffer, audioContext: AudioContext, time?
 
 export default class Metronome {
 
-  constructor() { };
-
-  private context = new AudioContext();
-  tempo = config.defaultTempo;
-  private samples: AudioBuffer[] | null = null;
-  private beat = 0;
   beatsPerBar: 2 | 3 | 4 = 4;
 
-  private timerId: ReturnType<typeof setTimeout> | undefined;
+  private _context = new AudioContext();
+  private _samples: AudioBuffer[] | null = null;
+  private _tempo = config.defaultTempo;
+  private _beat = 0;
+  private _timerId: ReturnType<typeof setTimeout> | undefined;
+
+  get tempo() {
+    return this._tempo;
+  }
+
+  set tempo(value) {
+    if (value < 1 || value > 1000) {
+      throw RangeError('Tempo value must must be between 1 and 1000');
+    }
+    this._tempo = value;
+  }
 
   remove() {
-    this.context.close();
+    this._context.close();
   }
 
   async loadSamples() {
     try {
-      this.samples = await loadSamples(['/audio/downbeat-click.wav', '/audio/beat-click.wav'], this.context)
+      this._samples = await loadSamples(['/audio/downbeat-click.wav', '/audio/beat-click.wav'], this._context)
       return 'success';
     }
     catch (error) {
@@ -44,21 +53,21 @@ export default class Metronome {
   }
 
   start() {
-    if (this.context.state === 'suspended') {
-      this.context.resume();
+    if (this._context.state === 'suspended') {
+      this._context.resume();
     }
     const scheduleAheadSec = config.scheduleAheadMilSec / 1000;
     // Init with values for immediate playback on first schedulePlayback call
-    this.beat = 0;
-    let beatDuration = 60 / this.tempo;
-    let nextNoteTime = this.context.currentTime;
+    this._beat = 0;
+    let beatDuration = 60 / this._tempo;
+    let nextNoteTime = this._context.currentTime;
     let prevNoteTime: number; // Value assigned during first schedulePlayback call
 
     const schedulePlayback = () => {
-      while (nextNoteTime <= this.context.currentTime + scheduleAheadSec) {
+      while (nextNoteTime <= this._context.currentTime + scheduleAheadSec) {
         // const sampleBuffer = (nextBeat === 0) ? downbeatBuffer : beatBuffer;
-        playSample(this.samples![1], this.context, nextNoteTime);
-        this.beat = (this.beat + 1) % this.beatsPerBar;
+        playSample(this._samples![1], this._context, nextNoteTime);
+        this._beat = (this._beat + 1) % this.beatsPerBar;
         prevNoteTime = nextNoteTime;
         nextNoteTime = prevNoteTime + beatDuration;
       };
@@ -68,15 +77,15 @@ export default class Metronome {
 
     const scheduleRepeatedly = () => {
       // Recalculate values according to currently set tempo
-      beatDuration = 60 / this.tempo;
+      beatDuration = 60 / this._tempo;
       nextNoteTime = prevNoteTime + beatDuration;
       schedulePlayback();
-      this.timerId = setTimeout(scheduleRepeatedly, config.scheduleAfterMilSec);
+      this._timerId = setTimeout(scheduleRepeatedly, config.scheduleAfterMilSec);
     };
 
     scheduleRepeatedly(); // Schedule subsequent notes on repeat
   }
   stop() {
-    clearTimeout(this.timerId);
+    clearTimeout(this._timerId);
   }
 }
