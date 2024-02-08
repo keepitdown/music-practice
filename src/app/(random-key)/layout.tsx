6689cea42db2e6172a7d6b6cf365f021164747f2
Artem <mntp112@yandex.ru>
2024-02-09 01:13:07 +0300

    feat: add hotkeys for metronome settings

--- src/app/(random-key)/layout.tsx
+++ src/app/(random-key)/layout.tsx
@@ -44,6 +44,34 @@ export default function RandomKeyLayout({ children, settings }: { children: Reac
     metronome.setIsTurnedOn(state => !state);
   };
 
+  const handleMetronomeHotKeys = useCallback((e: KeyboardEvent) => {
+    if (e.code === 'Space') {
+      metronome.setIsTurnedOn(state => !state);
+    }
+    if (e.code === 'ArrowUp') {
+      e.preventDefault();
+      setMetronomeSettings(draft => { draft.volume < MAX_VOLUME_LEVEL && draft.volume++ });
+    }
+    if (e.code === 'ArrowDown') {
+      e.preventDefault();
+      setMetronomeSettings(draft => { draft.volume > MIN_VOLUME_LEVEL && draft.volume-- });
+    }
+    if (e.code === 'ArrowLeft') {
+      e.preventDefault();
+      setMetronomeSettings(draft => { draft.tempo > MIN_TEMPO && draft.tempo-- });
+    }
+    if (e.code === 'ArrowRight') {
+      e.preventDefault();
+      setMetronomeSettings(draft => { draft.tempo < MAX_TEMPO && draft.tempo++ });
+    }
+  }, [metronome, setMetronomeSettings]);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleMetronomeHotKeys);
+    return () => {
+      window.removeEventListener('keydown', handleMetronomeHotKeys);
+    };
+  }, [handleMetronomeHotKeys]);
 
   return (
     <main>
